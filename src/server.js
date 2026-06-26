cc54aa70cfa89d9e8e57351a0c75a3d48b4ec83b
sessentaeseis <66entreaspasduplas@gmail.com>
2026-06-26 08:44:00 -0300

    sexta-feira

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 import express from 'express'
-import { prisma } from './lib/prisma'
+import { prisma } from './lib/prisma.ts'
+import cors from 'cors'
+
 const app = express()
 const PORT = 3000
 
+app.use(cors())
 app.use(express.json())
+
+app.get("/produtos", async (req, res) => {
+    const itens = await prisma.produto.findMany()
+    res.json(itens)
+}) 
 
 app.listen(PORT, () => {
     console.log("server rodando na porta " + PORT)
